4dd694d1959dd377420ed2f58fbf1c87b1d7b8bc
Evan Defloor <evandefloor@yahoo.com>
2024-05-09 23:14:30 -0400

    added ability to send params :D

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+use anyhow::Result;
 use clap::{App, Arg};
 use rand::Rng;
-use std::io::Result;
+use serde::{Deserialize, Serialize};
+// use serde_json::json;
+use std::error::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::time::{self, Duration};
 
+#[derive(Serialize, Deserialize, Debug)]
 struct CryptoParams {
+    #[serde(skip_serializing)]
+    #[serde(default)]
     a: u64,
     g: u64,
     p: u64,
     gamodp: u64,
+    #[serde(default)]
+    #[serde(skip_serializing)]
     sharedkey: u64,
 }
 
@@ -75,7 +83,7 @@ fn parse_port(port: &mut String, partner_port: &mut String) {
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let mut port: String = String::new();
     let mut partner_port: String = String::new();
     parse_port(&mut port, &mut partner_port);
@@ -84,14 +92,15 @@ async fn main() -> Result<()> {
     let params = CryptoParams::new(rand);
 
     let ip = format!("127.0.0.1:{}", port);
-    let partner_ip = format!("127.0.0.1:{}", port);
+    let partner_ip = format!("127.0.0.1:{}", partner_port);
     let mut rng = rand::thread_rng();
     // sleep for 5 or more seconds, which gives time for the attacker to impersonate one of them
     let timeout_duration = rng.gen_range(Duration::from_secs(5)..Duration::from_secs(15));
+    println!("Listening for {:?} seconds...", timeout_duration);
 
     let listener = TcpListener::bind(ip).await?;
     println!("Listening on port {}", port);
-
+    let serialized = serde_json::to_string(&params).unwrap();
     tokio::select! {
         // Handle incoming connections
         Ok((mut socket, _)) = listener.accept() => {
@@ -107,20 +116,19 @@ async fn main() -> Result<()> {
                             return;
                         }
                     };
-
-                    if let Err(e) = socket.write_all(&buf[0..n]).await {
-                        eprintln!("failed to write to socket; err = {:?}", e);
-                        return;
-                    }
+                    let deserialized: CryptoParams = serde_json::from_slice(&buf[..n]).expect("Failed to deserialize message");
+                    println!("{:?}", deserialized);
                 }
             });
         }
         // Timeout block
         _ = time::sleep(timeout_duration) => {
             println!("Timeout reached");
+            println!("Sending data to {}!", partner_ip);
             if let Ok(mut stream) = TcpStream::connect(partner_ip).await {
                 println!("Connected");
-                stream.write_all(b"Hello!").await?;
+                stream.write_all(serialized.as_bytes()).await?;
+                println!("Sent!");
             } else {
                 println!("Failed to connect");
             }
